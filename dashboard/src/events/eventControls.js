e5c05349d25afcebd627347c7a8f97405592b43a
Mac-lp3 <staticlp3@gmail.com>
2021-06-18 15:31:23 +1200

    basic delete function added

--- dashboard/src/events/eventControls.js
+++ dashboard/src/events/eventControls.js
@@ -6,17 +6,34 @@ class EventControls extends Component {
 
   constructor(props) {
     super(props)
+    this.state = {
+      eventArray: [{ id: 1}, { id: 2 }]
+    }
     this.deleteEvent = this.deleteEvent.bind(this)
+    this.state.eventRows = this.state.eventArray.map(e => <EventRow key={e.id} event={e} delete={this.deleteEvent}/>)
   }
 
-  deleteEvent(key) {
-    console.log(`deleting ${key}`)
+  deleteEvent(id) {
+
+    console.log(`deleting ${id}`)
+
+    this.setState((st, pr) => {
+      for(let i = 0; i < st.eventArray.length; ++i) {
+        if(st.eventArray[i].id === id) {
+          st.eventArray.splice(i, 1)
+          st.eventRows.splice(i, 1)
+          break
+        }
+      }
+      return {
+        eventArray: st.eventArray,
+        eventRows: st.eventRows
+      }
+    })
+    
   }
 
   render() {
-
-    const eventArray = [{ id: 1}, { id: 2 }]
-    const eventRows = eventArray.map(e => <EventRow key={e.id} event={e} delete={this.deleteEvent}/>)
 
     return (
       <table>
@@ -30,7 +47,7 @@ class EventControls extends Component {
           </tr>
         </thead>
         <tbody>
-            {eventRows}
+            {this.state.eventRows}
         </tbody>
       </table>
     )
